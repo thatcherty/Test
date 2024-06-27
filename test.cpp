086ca51dae5a05a63ebdcd162909e9fd1538221c
Thatcher King <ryeking19@gmail.com>
2024-06-27 14:45:02 -0800

    test commit to test branch

--- test.cpp
+++ test.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 int main() {
 
     cout << "This is a test, if you didn't know yet" << endl;
+
+    int n1, n2;
+
+    cout << "Enter two numbers: ";
+    cin >> n1 >> n2;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.clear();
+
+    cout << n1 << " + " << n2 << " = " << n1+n2 << endl;
 
     return 0;
 }
